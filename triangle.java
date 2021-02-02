67d210a6797341731747c0ee3995521f3fc1de8d
Rachamalla-Prasanth <rachamalla.prasanth@gmail.com>
2021-02-03 00:30:08 +0530

    Number 7 added in syso

--- triangle.java
+++ triangle.java
@@ -21,7 +21,7 @@ public class Triangle
 	}
 	public static void main(String[] args) 
 	{
-		System.out.println("Lower Traingle is as shown below:");
+		System.out.println("Lower Traingle is as shown below for input 7 as:");
 		draw(7);
 	}
 }
