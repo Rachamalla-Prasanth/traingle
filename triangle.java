1e1f4d3315d83457522737fdb8fa44c1345778da
Rachamalla-Prasanth <60349291+Rachamalla-Prasanth@users.noreply.github.com>
2021-03-25 17:32:37 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,24 +1,5 @@
 public class Triangle 
 {
-	public static  void draw(int n)
-	{
-		if(n>=3&&n<=99)
-		{
-			for(int i=0;i<n-1;i++)
-			{
-				for(int j=0;j<=i;j++)
-				{
-					System.out.print("");
-				}
-				for(int j=0;j<=n-1-i;j++)
-				{
-					System.out.print("*"+" ");
-				}
-				System.out.println();
-			}
-		}
-	System.out.println("*");
-	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Lower Traingle is as shown below for input 7 as:");
